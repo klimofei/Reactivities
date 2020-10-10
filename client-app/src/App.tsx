9d929ffab651f16b0332d4003a3cc02c9325170a
klimofei <klim.msu@gmail.com>
2020-10-10 11:14:28 -0400

    Third commit

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,34 +1,38 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { cars } from './demo';
-import { CarItem } from './Caritem'
+import {cars} from './demo';
+import {CarItem} from './Caritem'
 import axios from "axios";
+import {Header, Icon, List} from 'semantic-ui-react'
 
 class App extends Component {
-  state = {
-    values: []
-  }
+    state = {
+        values: []
+    }
 
-  componentDidMount() {
-    axios.get("http://localhost:5000/api/values")
-          .then((response) => {
-            console.log(response);
-            this.setState({
-            values: response.data })}
-            );
-  }
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <ul>
-            {this.state.values.map((value: any) => (
-              <li key={value.id}>{value.name}</li>
-            ))}
-          </ul>
-          {/* <p>
+    componentDidMount() {
+        axios
+            .get("http://localhost:5000/api/values")
+            .then((response) => {
+                this.setState({values: response.data})
+            });
+    }
+    render() {
+        return (
+            <div>
+                {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" /> */}
+                <Header as='h2'>
+                    <Icon name='users'/>
+                    <Header.Content>Reactivities</Header.Content>
+                </Header>
+                <List>
+                {this.state.values.map((value : any) => (
+                                <List.Item key={value.id}>{value.name}</List.Item>
+                            ))}
+                </List>
+                {/* <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <a
@@ -39,18 +43,19 @@ class App extends Component {
           >
             Learn React
           </a> */}
-        </header>
-  
-        {/* <ul>
+
+                {/* <ul>
           {/* {cars.map((car) => (
             <li>{car.manufacturer} {car.model}</li>
-            ))} */}
-          {/* {cars.map((car) => (
+            ))} */
+            }
+            {/* {cars.map((car) => (
           <CarItem car={car} />
             ))}
-        </ul> */}
-      </div>
-  
-  );}
+        </ul> */
+            } 
+    </div>
+    );
+  }
 }
 export default App;
